2339ec299eee7b6db5bd8894ad1037b718ca03ff
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2025-03-09 15:31:19 +0100

    refactor: Adjust ArticleGrid layout with column width constraint

--- src/components/ArticleGrid.tsx
+++ src/components/ArticleGrid.tsx
@@ -7,7 +7,7 @@ interface ArticleGridProps {
 
 const ArticleGrid: React.FC<ArticleGridProps> = ({ articles }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 columns-[300px]">
       {articles.map((article) => (
         <article
           key={article.id}
